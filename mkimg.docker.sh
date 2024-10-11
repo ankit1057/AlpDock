8eca8f85d862fc9c41f77542f8ccb9e7b2fdb610
Ankit <34769096+ankit1057@users.noreply.github.com>
2024-10-11 11:25:40 +0530

    Update mkimg.docker.sh

--- mkimg.docker.sh
+++ mkimg.docker.sh
@@ -4,7 +4,7 @@ profile_docker() {
 	profile_base
 	profile_abbrev="docker"
 	image_ext="iso"
-	arch="aarch64 x86 x86_64 ppc64le riscv64 s390x"
+	arch="aarch64 x86 x86_64 ppc64le riscv64 s390x i386"
 	output_format="iso"
 	kernel_addons="xtables-addons"
 	apks="$apks \
